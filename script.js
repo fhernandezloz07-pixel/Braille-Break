b6caa0ce97d5f113a06534541d39265536532eac
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-02-12 18:29:31 -0600

    Finished my general calculator by adding some more graphics and making it usable with the JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+//Get the display element 
+const outputBox = document.getElementById("outputBox");
 
+//Create 3 Functions:as chosen in our HTML
+  //1. One for "onScreen" 
+  function onScreen(input){ //The reason we put the input poarameter is because we are passing in a character when we call this functio
+    outputBox.value += input;
+  }
+  //2. Another for "evaluate"
+  function calculateResult(){
+    outputBox.value = eval(outputBox.value); //Evaluates the numbers, we can do this with the evaluation function
+  }
+  //3. And the last for "clearOutputBox"
+  function clearOutputBox(){
+      outputBox.value = "";
+  }
